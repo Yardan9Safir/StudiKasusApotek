6784e82f32ac81b1e890bddfaf27be26ac900eed
Yardan Safir <safir321ok@gmail.com>
2023-01-06 17:36:32 +0700

    Mengubah isi file MainApp.java

--- src/Main/MainApp.java
+++ src/Main/MainApp.java
@@ -6,8 +6,8 @@ import Views.StaffApotekPanel;
 public class MainApp {
 
     public static void main(String[] args) {
-//        LoginGUI test = new LoginGUI();
-        StaffApotekPanel p  = new StaffApotekPanel();
+        LoginGUI test = new LoginGUI();
+//        StaffApotekPanel p  = new StaffApotekPanel();
     }
 
 }
